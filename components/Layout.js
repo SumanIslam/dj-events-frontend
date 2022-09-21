744c282d9079cafea1665178b7e7a6e87c7b58ce
Suman Islam <sumanislam1416@gmail.com>
2022-09-21 23:12:27 +0600

    add header and footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
-import Head from 'next/head';
 import React from 'react';
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({ title, keywords, description, children }) => {
@@ -10,7 +12,9 @@ const Layout = ({ title, keywords, description, children }) => {
 				<meta name='description' content={description} />
 				<meta name='keywords' content={keywords} />
 			</Head>
+			<Header />
 			<div className={styles.container}>{children}</div>
+			<Footer />
 		</div>
 	);
 }
