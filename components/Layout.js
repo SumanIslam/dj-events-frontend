ae064931b4618e370eb8b4af674f18762a3bb2e9
Suman Islam <sumanislam1416@gmail.com>
2022-09-22 00:09:24 +0600

    add showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,29 @@
-import React from 'react';
+// dependencies
+import { useRouter } from 'next/router';
+// component
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 import styles from '@/styles/Layout.module.css'
 
 const Layout = ({ title, keywords, description, children }) => {
+	const router = useRouter();
   return (
 		<div>
+			{/* for SEO */}
 			<Head>
 				<title>{title}</title>
 				<meta name='description' content={description} />
 				<meta name='keywords' content={keywords} />
 			</Head>
+			{/* Header component */}
 			<Header />
+			{/* showcase component */}
+			{router.pathname === '/' && <Showcase />}
+			{/* all children component render here */}
 			<div className={styles.container}>{children}</div>
+			{/* footer component */}
 			<Footer />
 		</div>
 	);
