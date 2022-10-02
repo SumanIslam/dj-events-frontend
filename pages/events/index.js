ec2a1bfbec7485580e1939db46a5d7014d389ef0
Suman Islam <sumanislam1416@gmail.com>
2022-10-02 15:39:47 +0600

    get events data from strapi and display it in events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -9,16 +9,18 @@ export default function EventPage({ events }) {
 			<h1>Events</h1>
 			{events.length === 0 && <h3>No events to show</h3>}
 			{events.map((evt) => (
-				<EventItem key={evt.id} evt={evt} />
+				<EventItem key={evt.id} evt={evt.attributes} />
 			))}
 		</Layout>
 	);
 }
 
 export async function getStaticProps() {
-	const res = await fetch(`${API_URL}/api/events`);
+	const res = await fetch(`${API_URL}/api/events?populate=image&sort=date:asc`);
 
-	const events = await res.json();
+	const eventData = await res.json();
+
+	const events = eventData.data;
 
 	return {
 		props: { events },
